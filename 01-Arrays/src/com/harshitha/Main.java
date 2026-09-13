b7714f89cb79aed6f0182206eb7b39d3d67fa9e4
harshitha8988 <muddangulaharshitha@gmail.com>
2026-09-13 18:28:12 +0530

    Introduction to Arrays.

---
+++ 01-Arrays/src/com/harshitha/Main.java
@@ -0,0 +1,29 @@
+package com.harshitha;
+
+public class Main{
+    static void main() {
+        /* An Array is a collection of data of same type.
+            Syntax:
+                datatype[] var_name = new datatype[size];
+                                    or
+                 datatype[] var_name = {ele1,ele2,ele3,ele4];
+         */
+//        int[] rnos = new int[5];
+//        int[] rno = {23,45,34,54,22};
+//        System.out.println(rno[4]);
+
+//        int[] ros;   //declaration of array;defined in stack
+//        ros = new int[5];   //initialization of array;created in heap
+//        System.out.println(ros[1]);     //0
+
+        String[] arr = new String[5];
+        System.out.println(arr[1]);     //null
+
+
+
+
+
+
+
+    }
+}
